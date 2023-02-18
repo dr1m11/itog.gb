c4855687cdd4a6373eee4875b978eb820d009a7c
Marat <marat3389@gmail.com>
2023-02-18 16:55:45 +0300

    Написал код

--- code/Program.cs
+++ code/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = {"hello", "why", "no", "yes", "stop"};
+string result = "";
+
+for (int count = 0; count < array.Length; count++)
+{
+    if (array[count].Length <= 3)
+    {
+        result += array[count] + "_";
+    }
+}
+
+string[] newArray = result.Split("_");
+
+
+for (int i = 0; i < newArray.Length; i++) Console.Write(newArray[i] + " ");
